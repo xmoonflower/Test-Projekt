3757defddebd9f154526c106701b87e29338f06c
Nora <119288480+Thesleepinglaptopcat95@users.noreply.github.com>
2023-06-01 15:10:24 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,36 +1,58 @@
-const express = require('express')
-const cors = require('cors')
-const bodyParser = require('body-parser')
-const fs = require('fs')
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const fs = require('fs');
 
-const app = express()
-app.use(cors())
-app.use(bodyParser.json())
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
 
-const QUIZ_FILE = './quiz.json'
-
-// GET handler for /quiz endpoint to retrieve quiz questions
-app.get('/quiz', (req, res) => {
-  fs.readFile(QUIZ_FILE, (err, data) => {
-    if (err) throw err
-    const quiz = JSON.parse(data)
-    res.send(quiz)
-  })
-})
+const QUIZ_FILE = './quiz.json';
 
 // POST handler for /quiz endpoint to add a new quiz question
 app.post('/quiz', (req, res) => {
-  const quiz = req.body
-  fs.writeFile(QUIZ_FILE, JSON.stringify(quiz), err => {
-    if (err) throw err
-    res.send('Quiz question added successfully')
-  })
-})
+  const quizData = req.body;
+
+  fs.readFile(QUIZ_FILE, 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+      res.status(500).json({ success: false, message: 'Failed to read quiz data.' });
+    } else {
+      let quiz = [];
+      if (data) {
+        quiz = JSON.parse(data);
+      }
+      
+      quiz.push(quizData);
+      
+      fs.writeFile(QUIZ_FILE, JSON.stringify(quiz), 'utf8', (err) => {
+        if (err) {
+          console.error(err);
+          res.status(500).json({ success: false, message: 'Failed to add quiz question.' });
+        } else {
+          res.json({ success: true, message: 'Quiz question added successfully.' });
+        }
+      });
+    }
+  });
+});
+
+// GET handler for /quiz/play endpoint to play the quiz
+app.get('/quiz/play', (req, res) => {
+  fs.readFile(QUIZ_FILE, 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+      res.status(500).json({ success: false, message: 'Failed to read quiz data.' });
+    } else {
+      const quiz = JSON.parse(data);
+      res.json({ success: true, quiz: quiz });
+    }
+  });
+});
 
 app.listen(7777, () => {
-  console.log('Quiz app server listening on port 7777')
-})
-})
+  console.log('Quiz app server listening on port 7777');
+});
 
 
 
