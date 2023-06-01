f874aae84fc635c9ae5ab1b9db77bca9e8a0c3f5
Nora <119288480+Thesleepinglaptopcat95@users.noreply.github.com>
2023-06-01 11:02:50 +0200

    Quizmaker

--- server.js
+++ server.js
@@ -1,11 +1,32 @@
-const http = require('http');
+const express = require('express')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const fs = require('fs')
 
-const server = http.createServer((request, response) => {
-  response.writeHead(200, {'Content-Type': 'text/plain'});
-  response.end('Hello World!');
-});
+const app = express()
+app.use(cors())
+app.use(bodyParser.json())
 
-server.listen(7777, () => {
-  console.log('Server running on port 7777');
-});
+const QUIZ_FILE = './quiz.json'
 
+// GET handler for /quiz endpoint to retrieve quiz questions
+app.get('/quiz', (req, res) => {
+  fs.readFile(QUIZ_FILE, (err, data) => {
+    if (err) throw err
+    const quiz = JSON.parse(data)
+    res.send(quiz)
+  })
+})
+
+// POST handler for /quiz endpoint to add a new quiz question
+app.post('/quiz', (req, res) => {
+  const quiz = req.body
+  fs.writeFile(QUIZ_FILE, JSON.stringify(quiz), err => {
+    if (err) throw err
+    res.send('Quiz question added successfully')
+  })
+})
+
+app.listen(7777, () => {
+  console.log('Quiz app server listening on port 7777')
+})
