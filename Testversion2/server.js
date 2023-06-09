1abbe2e69bafa6af1a1360c4c1169b3f9ffcb4ca
Nora <119288480+Thesleepinglaptopcat95@users.noreply.github.com>
2023-06-09 12:10:48 +0200

    Update server.js

--- Testversion2/server.js
+++ Testversion2/server.js
@@ -1,9 +1,46 @@
 const express = require('express');
 const app = express();
-const port = 7799;
+const fs = require('fs');
 
-app.use(express.static('public'));
+// Middleware zum Parsen von JSON-Daten
+app.use(express.json());
 
-app.listen(port, () => {
-  console.log(`Server läuft auf Port ${port}`);
+// GET-Route für das Abrufen der aktuellen Fragenliste
+app.get('/fragen', (req, res) => {
+  fs.readFile('fragen.json', 'utf8', (err, data) => {
+    if (err) {
+      res.status(500).json({ error: 'Fehler beim Lesen der Fragen.' });
+    } else {
+      res.json(JSON.parse(data));
+    }
+  });
+});
+
+// POST-Route zum Hinzufügen einer neuen Frage
+app.post('/frage-hinzufuegen', (req, res) => {
+  fs.readFile('fragen.json', 'utf8', (err, data) => {
+    if (err) {
+      res.status(500).json({ error: 'Fehler beim Lesen der Fragen.' });
+    } else {
+      const questions = JSON.parse(data);
+      questions.push(req.body);
+
+      fs.writeFile('fragen.json', JSON.stringify(questions), 'utf8', (err) => {
+        if (err) {
+          res.status(500).json({ error: 'Fehler beim Speichern der Frage.' });
+        } else {
+          res.json({ message: 'Die Quizfrage wurde erfolgreich erstellt und gespeichert.' });
+        }
+      });
+    }
+  });
+});
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/make-quiz.html');
+});
+
+// Server starten
+app.listen(7799, () => {
+  console.log('Der Server ist gestartet und hört auf dem Port 7799.');
 });
